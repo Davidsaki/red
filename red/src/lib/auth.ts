8d65627ce2f0e39a48dff1719722530cdbb4ea36
David Cruz <davidsaki@gmail.com>
2025-12-01 23:44:02 -0500

    adding types

--- red/src/lib/auth.ts
+++ red/src/lib/auth.ts
@@ -11,7 +11,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     async session({ session, token }) {
       // Agregar información adicional a la sesión
-      if (token.sub) {
+      if (token.sub && session.user) {
         session.user.id = token.sub;
       }
       return session;
